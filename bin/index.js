07c1f15a79d3ae77c57b336f120784aed4dfd4d7
Sherazi <sherazahmad720@gmail.com>
2024-02-02 23:49:45 +0500

    Refactor user input validation and increment attempts

--- bin/index.js
+++ bin/index.js
@@ -2,14 +2,11 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 import chalkAnimation from 'chalk-animation';
-
 let secretNumber = Math.floor(Math.random() * 10) + 1; // Random number between 1 and 10
 let attempts = 0;
 const welcomeMessage = chalkAnimation.rainbow('Welcome to the Number Guessing Game! by Sherazi');
-
 const playGame = async () => {
     resetGame();
-   
     console.log(chalk.blue('Please guess a number between 1 and 10. You have 3 attempts.'));
     while (true) {
         const { userGuess } = await inquirer.prompt({
@@ -17,34 +14,28 @@ const playGame = async () => {
             name: 'userGuess',
             message: 'Guess a number:',
         });
-
-        
+        attempts++;
         if (isNaN(userGuess)) {
             console.log(chalk.red('Invalid input. Please enter a number.'));
-            
             continue;
         }
-        attempts++;
-
         if (userGuess < secretNumber) {
             console.log(chalk.magentaBright(`Too low! Try again.\n You have ${3 - attempts} attempts left.`));
-
-        } else if (userGuess > secretNumber) {
+        }
+        else if (userGuess > secretNumber) {
             console.log(chalk.magentaBright(`Too high! Try again. \n You have ${3 - attempts} attempts left.`));
-        } else {
+        }
+        else {
             console.log(chalk.green(`Congratulations! You guessed the correct number ${secretNumber} in ${attempts} attempts.`));
-
             chalkAnimation.neon("-------------------------------------------------------------------------- \n                    Congratulations! You guessed it!\n-------------------------------------------------------------------------- ").start();
             exitConfirmation();
             break;
         }
-
         if (attempts >= 3) {
             console.log(chalk.red(`Sorry, you've reached the maximum number of attempts. The correct number was ${secretNumber}.`));
             chalkAnimation.neon('Game Over').start();
             exitConfirmation();
             break;
-
         }
     }
 };
@@ -52,9 +43,7 @@ function resetGame() {
     attempts = 0;
     secretNumber = Math.floor(Math.random() * 10) + 1;
 }
-
 // playGame();
-
 async function welcome() {
     welcomeMessage.start();
     setTimeout(() => {
@@ -62,7 +51,6 @@ async function welcome() {
         playGame();
     }, 1000);
 }
-
 function exitConfirmation() {
     inquirer.prompt([
         {
@@ -73,7 +61,8 @@ function exitConfirmation() {
     ]).then((answers) => {
         if (answers.repeat) {
             playGame();
-        } else {
+        }
+        else {
             console.log('Thanks for playing!');
             process.exit();
         }
